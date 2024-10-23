c3c44fcaf7241c0a94a378192ed32491b0628f57
Niklas <Niklasgth@hotmail.com>
2024-10-23 18:21:50 +0200

    giiiii

--- TodoThyme/src/main/java/com/todoThyme/TodoThyme/TodoController.java
+++ TodoThyme/src/main/java/com/todoThyme/TodoThyme/TodoController.java
@@ -5,27 +5,45 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import ch.qos.logback.core.model.Model;
-
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 public class TodoController {
 
 private static final List<Todo> todoItems = new ArrayList<>();
-static {
-    todoItems.add(new Todo("Skriv ut listan", 1));
-    todoItems.add(new Todo("Lägga till nytt", 2));
-    todoItems.add(new Todo("Ta bort från listan", 3));
-    todoItems.add(new Todo("Stäng ner och gå hem", 4));
-}
+    /*static {
+        todoItems.add(new Todo("Skriv ut listan", 1));
+        todoItems.add(new Todo("Lägga till nytt", 2));
+        todoItems.add(new Todo("Ta bort från listan", 3));
+        todoItems.add(new Todo("Stäng ner och gå hem", 4));
+}*/
 
 
     @GetMapping("/todo")
         String getTodo(Model model) {
              model.addAttribute("todoItems",todoItems);
+             model.addAttribute("newTodo", new Todo(null, 0));
          return  "todo";
         }
 
+    
+
+    @PostMapping("/new-item")
+    String newItem(@RequestParam("name") String name){
+        System.out.println("PostMapping "+ name );
+        todoItems.add(new Todo(name, todoItems.size() +1 ));
+        return "redirect:/todo";
     }
+
+@GetMapping("/remove-item/{itemId}")
+String removeItem(@PathVariable int itemId) {
+    System.out.println("remove item "+itemId);
+    todoItems.removeIf(item -> item.getId() == itemId);
+    return "redirect:/todo";
+}
+
+
+}
